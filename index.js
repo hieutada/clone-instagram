f7b9bb3c5753138bab690766ad260f2634e9fcb1
Ta Trung Hieu <hieutada2k@gmail.com>
2021-12-20 08:14:30 +0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.set('layout', './layout');
 app.get('/', (req, res) =>
   res.render('home', { active: ['fas', 'fal', 'fal', 'fal', 'fal'] })
 );
-app.get('/inbox', (req, res) => res.render('inbox', { active: ['fal', 'fas', 'fal', 'fal', 'fal'] }));
-app.get('/explore', (req, res) => res.render('explore', { active: ['fal', 'fal', 'fal', 'fas', 'fal'] }));
+app.get('/inbox', (req, res) =>
+  res.render('inbox', { active: ['fal', 'fas', 'fal', 'fal', 'fal'] })
+);
+app.get('/explore', (req, res) =>
+  res.render('explore', { active: ['fal', 'fal', 'fal', 'fas', 'fal'] })
+);
+app.get('/profile', (req, res) =>
+  res.render('profile')
+);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
